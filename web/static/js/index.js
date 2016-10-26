9530d6e34101d11bab45719b48ce7e4317733b44
Bruno Antunes <sardaukar.siet@gmail.com>
2016-10-26 20:38:40 +0100

    basic React setup

---
+++ web/static/js/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import CounterApp from './containers/counterApp';
+import configureStore from './store/configureStore';
+import {setCounter} from './actions/counter';
+
+const store = configureStore();
+
+export default class Root extends Component {
+  componentWillMount() {
+    store.dispatch(setCounter(this.props.counter));
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <CounterApp />
+      </Provider>
+    );
+  }
+}
+
+render(
+  <Root />, document.getElementById('root')
+);
---
+++ web/static/js/store/configureStore.js
@@ -0,0 +1,11 @@
+import { compose, createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+
+const createStoreWithMiddleware = compose(applyMiddleware(
+  thunk
+), window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
+
+export default function configureStore(initialState) {
+  return createStoreWithMiddleware(rootReducer, initialState);
+}
