89dddb21832fd500fc8c72434868ba49dc7b4555
Bruno Antunes <sardaukar.siet@gmail.com>
2016-10-29 19:27:54 +0100

    cleanup

--- web/static/js/middleware/socketMiddleware.js
+++ web/static/js/middleware/socketMiddleware.js
@@ -1,54 +1,29 @@
 import { Socket } from 'phoenix';
 import * as CounterActions from '../actions/counter';
+import * as WsActions from '../actions/websocket';
 
 const socketMiddleware = (function(){
   var socket = null;
   var channel = null;
 
-  const onOpen = (ws,store,token) => evt => {
-    //Send a handshake, or authenticate with remote end
-
-    console.log("ON OPEN");
-    //Tell the store we're connected
-    //store.dispatch(actions.connected());
-  }
-
-  const onClose = (ws,store) => evt => {
-    //Tell the store we've disconnected
-    //store.dispatch(actions.disconnected());
-    console.log("ON CLOSE");
-  }
-
   const onIncrementFromServer = (msg,store) => evt => {
-    console.log('OM: ', msg);
     store.dispatch(CounterActions.incrementFromServer());
-  }
-
-  const onChannelMessage = (evt, cb, store) => evt => {
-    console.log("OCM", evt, cb, store);
   }
 
   return store => next => action => {
 
     switch(action.type) {
-      case 'CONNECT':
-        //Start a new connection to the server
+      case WsActions.CONNECT:
         if(socket != null) {
           socket.close();
         }
-        //Send an action that shows a "connecting..." status for now
-        //store.dispatch(actions.connecting());
 
-        //Attempt to connect (we could send a 'failed' action on error)
         socket = new Socket('/ws', {
           logger: (kind, msg, data) => {
             console.log(`${kind.toUpperCase()} msg: ${msg}`);
             console.log('data:', data);
           }
         });
-
-        socket.onClose = onClose(socket,store);
-        socket.onOpen = onOpen(socket,store,action); // FIXME: action.token
 
         socket.connect();
         channel = socket.channel('counter:1');
@@ -63,18 +38,16 @@ const socketMiddleware = (function(){
           .receive('timeout', () => console.log('Networking issue. Still waiting...'));
 
         break;
-      //The user wants us to disconnect
-      case 'DISCONNECT':
+
+      case WsActions.DISCONNECT:
         if(socket != null) {
           socket.close();
         }
         socket = null;
 
-        //Set our state to disconnected
-        //store.dispatch(actions.disconnected());
         break;
 
-      case 'INCREMENT_ON_SERVER':
+      case CounterActions.INCREMENT_ON_SERVER:
         console.log('sending action to server...');
         channel.push('counter:async', {text: 'meh'})
           .receive('ok', _resp => {
@@ -82,9 +55,9 @@ const socketMiddleware = (function(){
           })
           .receive('error', error => {
             console.log('server errored out', error);
-            //store.dispatch(CounterActions.serverError(error));
+            //store.dispatch(serverError(error));
           })
-        return next(action);;
+        return next(action);
 
       default:
         return next(action);
