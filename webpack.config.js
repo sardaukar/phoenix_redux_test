8b1b27dcf393f2252e54cccb62a71fafa88675ba
Bruno Antunes <sardaukar.siet@gmail.com>
2016-10-29 10:48:12 +0100

    proper CSS(SASS) handling for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,43 +5,49 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var env = process.env.MIX_ENV || 'dev';
 var isProduction = (env === 'prod');
 
-var plugins = [new ExtractTextPlugin('app.css')];
+var plugins = [new ExtractTextPlugin('css/app.css')];
 
 // This is necessary to get the sass @import's working
 var stylePathResolves = (
-    'includePaths[]=' + path.resolve('./') + '&' +
-    'includePaths[]=' + path.resolve('./node_modules')
-  );
+  'includePaths[]=' + path.resolve('./') + '&' +
+  'includePaths[]=' + path.resolve('./node_modules')
+);
 
 if (isProduction) {
   plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
 }
 
 module.exports = {
-  entry: './web/static/js/index.js',
+  entry: [
+    './web/static/js/index.js',
+    './web/static/css/base.sass'
+  ],
   output: {
-    path: './priv/static/js',
-    filename: 'app.js'
+    path: './priv/static',
+    filename: 'js/app.js'
+  },
+  resolve: {
+    modulesDirectories: [ "node_modules", __dirname + "/web/static/js" ]
   },
   module: {
-        loaders: [
-            {
-              test: /\.jsx?$/,
-              exclude: /(node_modules|bower_components)/,
-              loader: 'babel',
-              query: {
-                presets: ['es2015', 'react']
-              }
-            },
-            {
-              test: /\.scss$/,
-              loader: ExtractTextPlugin.extract(
-                'style',
-                'css' + '!sass?outputStyle=expanded&' + stylePathResolves
-              )
-            }
+    loaders: [
+        {
+          test: /\.js?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015', 'react']
+          }
+        },
+        {
+          test: /\.sass$/,
+          loader: ExtractTextPlugin.extract(
+            'style',
+            'css' + '!sass?outputStyle=expanded&' + stylePathResolves
+          )
+        }
 
-        ]
+    ]
   },
   plugins: plugins
 };
